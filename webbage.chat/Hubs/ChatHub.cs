6ed29cecd5dbee072c03a8134962cac3c637139a
bamblack <benjamin.a.m.black@gmail.com>
2014-06-14 21:56:41 -0400

    Fixed message spacing issue

--- webbage.chat/Hubs/ChatHub.cs
+++ webbage.chat/Hubs/ChatHub.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace webbage.chat.Hubs {
     public class ChatHub : Hub {
 
         public void Send(string name, string message) {
             Clients.All.addNewMessageToPane(name, message);
+        }
+
+        public override Task OnConnected() {           
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected() {
+            return base.OnDisconnected();
         }
 
     }
