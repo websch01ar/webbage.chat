e4f5ee1af6d7202f4b4fc681db26081ad034a3de
Ben <benjamin.a.m.black@gmail.com>
2014-06-16 13:37:19 -0400

    Starting work on webbage.chat.bot

--- webbage.chat/Hubs/ChatHub.cs
+++ webbage.chat/Hubs/ChatHub.cs
@@ -13,44 +13,19 @@ namespace webbage.chat.Hubs {
 
         public void Send(string name, string message) {
             Clients.All.addNewMessageToPane(name, message);
-        }
 
+            if (message.StartsWith("!")) {
+                interpretCommand(message);
+            }
+        }
         public void UpdateOnlineUsers() {
             var jsonUsers = (new JavaScriptSerializer()).Serialize(onlineUsers);
             Clients.All.updateOnlineUsers(jsonUsers);
         }
-
         public void RemoveDisconnectedUser(string name) {
             Clients.All.removeDisconnectedUser(name);
         }
-
-        public override Task OnConnected() {
-            // get the userName from the querystring set in jquery, send a notification
-            // from room that the user has joined
-            string user = GetUser();
-            Send("room", string.Format("{0} has joined the room", user));           
-
-            onlineUsers.Add(Context.ConnectionId, user);
-
-            // TODO: Update #onlineUserList
-            UpdateOnlineUsers();
-
-            return base.OnConnected();
-        }
-
-        public override Task OnDisconnected() {
-            string user = GetUser();
-            
-            // find them in the dictionary
-            var disconnectedUser = onlineUsers.Where(u => u.Key == user || u.Value == user).FirstOrDefault();
-            Send("room", string.Format("{0} has left the building", disconnectedUser.Value));
-
-            onlineUsers.Remove(disconnectedUser.Key);
-            RemoveDisconnectedUser(disconnectedUser.Value);
-            return base.OnDisconnected();
-        }
-
-        private string GetUser() {
+        private string getUser() {
             string clientId = "";
             if (!(Context.QueryString["userName"] == null))
                 clientId = Context.QueryString["userName"].ToString();
@@ -60,6 +35,33 @@ namespace webbage.chat.Hubs {
 
             return clientId;
         }
+        private void interpretCommand(string command) {
+
+        }
+
+        public override Task OnConnected() {
+            // get the userName from the querystring set in jquery, send a notification
+            // from room that the user has joined
+            string user = getUser();
+            Send("room", string.Format("{0} has joined the room", user));           
+
+            onlineUsers.Add(Context.ConnectionId, user);
+
+            UpdateOnlineUsers();
+
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected() {
+            string user = getUser();
+            
+            // find them in the dictionary
+            var disconnectedUser = onlineUsers.Where(u => u.Key == user || u.Value == user).FirstOrDefault();
+            Send("room", string.Format("{0} has left the building", disconnectedUser.Value));
+
+            onlineUsers.Remove(disconnectedUser.Key);
+            RemoveDisconnectedUser(disconnectedUser.Value);
+            return base.OnDisconnected();
+        }        
 
     }
 }
