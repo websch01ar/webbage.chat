bee7a39bef6e97d2f0c476f6b10e944040f1a707
Ben <benjamin.a.m.black@gmail.com>
2014-06-16 18:10:35 -0400

    Checking in Command and CommandStruct
    Get botCommands working and start login on roomCommands

---
+++ webbage.chat.bot/Command.cs
@@ -0,0 +1,12 @@
+﻿
+namespace webbage.chat.bot {
+    public class Command {
+        public string Text { get; set; }
+        public string[] Args { get; set; }
+        public string Response { get; set; }
+
+        public Command(string commandText) {
+            this.Text = commandText;
+        }
+    }
+}
---
+++ webbage.chat.bot/CommandStruct.cs
@@ -0,0 +1,18 @@
+﻿
+namespace webbage.chat.bot {
+    public struct CommandStruct<TDesc, TParser, TAction> {
+        private TDesc desc;
+        private TParser parser;        
+        private TAction action;
+
+        public CommandStruct(TParser parser, TDesc desc, TAction action) {
+            this.desc = desc;
+            this.parser = parser;            
+            this.action = action;
+        }
+
+        public TDesc Desc { get { return desc; } set { desc = value; } }
+        public TParser Parser { get { return parser; } set { parser = value; } }        
+        public TAction Action { get { return action; } set { action = value; } }
+    }
+}
