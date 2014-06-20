f3a23997aee4eaa64860ab8df309eb811a1677f3
Ben <benjamin.a.m.black@gmail.com>
2014-06-20 16:47:16 -0400

    Minifying js

--- webbage.chat/App_Start/BundleConfig.cs
+++ webbage.chat/App_Start/BundleConfig.cs
@@ -5,28 +5,22 @@ namespace webbage.chat {
     public class BundleConfig {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.title-alert.js"));
+            bundles.Add(new ScriptBundle("~/scripts/jquery").Include(
+                        "~/Scripts/jquery-{version}.min.js",
+                        "~/Scripts/site.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/respond.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/scripts/chat").Include(
+                        "~/Scripts/chat.js",
+                        "~/Scripts/jquery.signalR-2.0.3.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/colors.css"));
-
-            bundles.Add(new StyleBundle("~/Content/chat").Include(
+                      "~/Content/colors.css", 
                       "~/Content/chat.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
