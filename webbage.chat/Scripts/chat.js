34f4cb1251e5d2120a1c20e5670dbc0348c2ecaf
Ben <benjamin.a.m.black@gmail.com>
2014-06-24 13:23:44 -0400

    Resolves #7 tab notifications now working

--- webbage.chat/Scripts/chat.js
+++ webbage.chat/Scripts/chat.js
@@ -1,4 +1,59 @@
-﻿$(function () {
+﻿// page visible/hidden logic
+(function () {
+    var hidden = "hidden";
+
+    // Standards:
+    if (hidden in document)
+        document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+        document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+        document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+        document.addEventListener("msvisibilitychange", onchange);
+        // IE 9 and lower:
+    else if ('onfocusin' in document)
+        document.onfocusin = document.onfocusout = onchange;
+        // All others:
+    else
+        window.onpageshow = window.onpagehide
+            = window.onfocus = window.onblur = onchange;
+
+    function onchange(evt) {
+        var v = 'visible', h = 'hidden',
+            evtMap = {
+                focus: v, focusin: v, pageshow: v, blur: h, focusout: h, pagehide: h
+            };
+
+        evt = evt || window.event;
+        if (evt.type in evtMap)
+            document.body.className = evtMap[evt.type];
+        else
+            document.body.className = this[hidden] ? "hidden" : "visible";
+        
+        if (document.body.className == 'visible')
+            resetTitle();
+    }
+})();
+
+// tab notification logic
+var oldTitle, notificationCount = 0, firstTime = true;
+function updateTitle() {
+    if (firstTime) {
+        firstTime = false;
+        oldTitle = document.title;
+    }
+
+    notificationCount++;
+    document.title = '(' + notificationCount + ') ' + oldTitle;
+};
+function resetTitle() {
+    firstTime = true;
+    document.title = oldTitle;
+    notificationCount = 0;
+}
+
+$(function () {
     var chat            = $.connection.chatHub;
     var $displayName    = $('#displayName');
     var $chatDisplay    = $('#chat');    
@@ -83,6 +138,10 @@
         var $ele = $('.chat-room-message-message.' + messageId);
         if (needsShowMore($ele)) {
             $('#chat :last-child').children('.messages').append('<div id="show-' + messageId + '" class="show-more">{show full text}</div>');
+        }
+
+        if (document.body.className == 'hidden' && name != 'room') {
+            updateTitle();
         }
     };
     ///////////////////////////////////////////////////////////////////////////////  
