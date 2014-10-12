42f22750ada332b887ddec956cb5f2547d98bc5d
bamblack <benjamin.a.m.black@gmail.com>
2014-10-11 20:23:36 -0400

    Clearing out dev branch

--- webbage.chat/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webbage.chat.MvcApplication" Language="C#" %>
--- webbage.chat/Scripts/site.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $('.chat-room-display').click(function () {        
-        // get the chatroom attribute
-        location.replace("rooms/" + $(this).data('chatroom'));
-    });
-});
