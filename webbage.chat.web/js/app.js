02e1a9a3b26bd37aa25f9d1323b544daf7a7e85e
Ben Black <ben.black@troutmansanders.com>
2014-10-14 09:43:23 -0400

    Testing logout

--- webbage.chat.web/js/app.js
+++ webbage.chat.web/js/app.js
@@ -19,6 +19,7 @@
         });
     }).run(function($rootScope) {
         $rootScope.signout = function () {
+            alert(authProvider);
             authProvider.signout();
         }
     });
