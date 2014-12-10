356b370ec2b1245e3884a1813f9b0045e98758e8
Ben Black <ben.black@troutmansanders.com>
2014-12-10 17:08:50 -0500

    Attempting to get Auth0 to work

--- webbage.chat.web/app/webbage.app/controllers/login.js
+++ webbage.chat.web/app/webbage.app/controllers/login.js
@@ -3,9 +3,9 @@
 
     angular.module('webbage.app').controller('login', login);
 
-    login.$inject = ['$scope', 'store', '$location', 'auth'];
+    login.$inject = ['$scope', 'store', '$location'];
 
-    function login($scope, store, $location, auth) {
+    function login($scope, store, $location) {
         auth.signin({}, function (profile, token) {
             store.set('profile', profile);
             store.set('token', token);
